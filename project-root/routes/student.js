598d0b445fecd1eeaca494773c2bef43fa30a572
Aditya_Contributes <146584952+Dev-Aditya-More@users.noreply.github.com>
2024-12-23 23:40:00 +0530

    Delete project-root directory

--- project-root/routes/student.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTests, submitTest } = require('../controllers/studentController');
-
-// Student routes
-router.get('/tests', getTests);
-router.post('/tests/:id/submit', submitTest);
-
-module.exports = router;
